e43ecd97bc590cf940f33972bcccbcc273e449c9
Frances <frances.deboo@gmail.com>
2020-11-17 21:01:41 +0000

    Models created for products and data fixtures loaded

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
+from .models import Product, Category, AddOn, Insurance, Destination
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = (
+        'product_id',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class AddOnAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'name',
+        'category',
+        'price',
+        'image',
+        'min_medical_threshold',
+    )
+
+    ordering = (
+        'product_id',
+    )
+
+
+class InsuranceAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'name',
+        'category',
+        'price',
+        'image',
+        'friendly_name',
+    )
+
+    ordering = (
+        'product_id',
+    )
+
+
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'name',
+        'max_passengers',
+        'duration',
+        'min_medical_threshold',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = (
+        'product_id',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(AddOn, AddOnAdmin)
+admin.site.register(Insurance, InsuranceAdmin)
+admin.site.register(Destination, DestinationAdmin)
--- products/models.py
+++ products/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     product_id = models.CharField(max_length=254)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
     image_thumb = models.ImageField(null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -35,7 +35,9 @@ class Product(models.Model):
 
 
 class AddOn(Product):
-    min_medical_threshold = models.CharField(max_length=75, blank=True)
+    min_medical_threshold = models.IntegerField(
+        default=0, null=False, blank=False
+    )
 
     def __str__(self):
         return self.name
@@ -43,6 +45,17 @@ class AddOn(Product):
 
 class Insurance(Product):
     friendly_name = models.CharField(max_length=75, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Destination(Product):
+    max_passengers = models.IntegerField(null=True, blank=True)
+    duration = models.CharField(max_length=20, blank=True)
+    min_medical_threshold = models.IntegerField(
+        default=0, null=False, blank=False
+    )
 
     def __str__(self):
         return self.name
