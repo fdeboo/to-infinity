939cabbe239a51816cca1489195ce9d2f7a4da89
Frances <frances.deboo@gmail.com>
2020-11-18 19:59:57 +0000

    Added simple search bar

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -6,6 +8,20 @@ def all_trips(request):
     """ A view to show all trips """
     template = 'products/trips.html'
     destinations = Product.objects.filter(category=3)
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!"
+                    )
+                return redirect(reverse('trips'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query
+                )
+            destinations = destinations.filter(queries)    
 
     context = {
         "destinations": destinations,
@@ -15,7 +31,7 @@ def all_trips(request):
 
 
 def trip_detail(request, product_id):
-    """ A view to show individual event details """
+    """ A view to show individual destination details """
     destination = get_object_or_404(Product, pk=product_id)
 
     template = 'products/trip_detail.html'
