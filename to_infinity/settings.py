96f472b5c15bb4f27d5a170a9df77718aec72e53
Frances <frances.deboo@gmail.com>
2020-12-22 16:21:48 +0000

    Added intcomma to format totals with thousands separaters

--- to_infinity/settings.py
+++ to_infinity/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.humanize',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
