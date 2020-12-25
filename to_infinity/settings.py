eb5ef64d46f8c8c38e1eecaa3633e973dafec1a1
Frances <frances.deboo@gmail.com>
2020-12-25 01:21:11 +0000

    Typo in settings.py

--- to_infinity/settings.py
+++ to_infinity/settings.py
@@ -181,7 +181,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'fdeboo-boutique-ado'
+    AWS_STORAGE_BUCKET_NAME = 'toinfinity'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
