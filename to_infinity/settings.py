2283de53b1e8616c9a95ee0110c7e23f646d5601
Frances <frances.deboo@gmail.com>
2020-12-26 16:47:19 +0000

    Added email functionality

--- to_infinity/settings.py
+++ to_infinity/settings.py
@@ -205,14 +205,14 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
-# if 'DEVELOPMENT' in os.environ:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = '2infinity@example.com'
-"""else:
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'toinfinity@example.com'
+else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_USER = os.environ.get('EMAIL_HOST_USER')"""
+    DEFAULT_FROM_USER = os.environ.get('EMAIL_HOST_USER')
